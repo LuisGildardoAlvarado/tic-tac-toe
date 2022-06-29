2b69d41632d3cc8fa6a8d800aab1911e400c1637
LuisGildardoAlvarado <luisgil9900@gmail.com>
2022-06-28 18:34:40 -0700

    tic-tac-toe funcionando

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
+import ScoreBoard from './components/ScoreBoard';
 import TDLogo from './components/TDLogo';
+
+const winningPositions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+];
 
 function App() {
 
   const [turn, setTurn] = useState("x");
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winningSquares, setWinningSquares] = useState([]);
   const[score, setScore] = useState({
     x:0,
     o:0,
   });
 
-  const checkForWinner = squares => {
-    setTurn(turn ==="x" ? "o" : "x");
+  const reset = () => {
+    setTurn('x');
+    setSquares(Array(9).fill(null));
+    setWinningSquares([]);
+  }
+
+  const checkForWinner = newSquares => {
+    for(let i = 0; i < winningPositions.length; i++) {
+      const [a,b,c] = winningPositions[i];
+      if(newSquares[a] && newSquares[a] === newSquares[b] && newSquares[a] === newSquares[c]) {
+        endGame(newSquares[a], winningPositions[i]);
+        return
+      }
+    }
+
+    if(!newSquares.includes(null)) {
+      endGame(null, Array.from(Array(10).keys()));
+      return
+    }
+    setTurn(turn === 'x' ? 'o' : 'x');
   }
 
   const handleClick= square =>{
@@ -23,10 +54,24 @@ function App() {
     checkForWinner(newSquares);
   }
 
+  const endGame = (result, winningPositions) => {
+    setTurn(null);
+    if(result !== null) {
+      setScore({
+        ...score,
+        [result]: score[result] + 1,
+      })
+    }
+    setWinningSquares(winningPositions);
+    setTimeout(reset, 3000);
+    
+  }
+
   return (
     <div className="App">
       <TDLogo />
-      <Board turn={turn} squares={squares} onClick={handleClick}/>
+      <Board winningSquares={winningSquares} turn={turn} squares={squares} onClick={handleClick}/>
+      <ScoreBoard scoreX={score.x} scoreO={score.o} />
     </div>
   );
 }
---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,10 @@
+import '../styles/ScoreBoard.css'
+
+const ScoreBoard = ( {scoreX, scoreO} ) => (
+    <div className='score-board'>
+        <div className='scoreX'>{scoreX}</div>
+        <div className='scoreO'>{scoreO}</div>
+    </div>
+)
+
+export default ScoreBoard;
